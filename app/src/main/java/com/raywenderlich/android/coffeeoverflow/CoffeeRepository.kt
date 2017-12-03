387dd0185985b0eff1246b2dbf99a844c7bf5575
Victoria Gonda <v.m.gonda@gmail.com>
2017-12-03 14:15:38 -0500

    Add FAB to increment coffees

---
+++ app/src/main/java/com/raywenderlich/android/coffeeoverflow/CoffeeRepository.kt
@@ -0,0 +1,9 @@
+package com.raywenderlich.android.coffeeoverflow
+
+class CoffeeRepository {
+  private var coffees = 0
+
+  fun increment() {
+    coffees ++
+  }
+}
--- app/src/main/java/com/raywenderlich/android/coffeeoverflow/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/coffeeoverflow/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.raywenderlich.android.coffeeoverflow
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+  private val coffeeRepo = CoffeeRepository()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    addCoffee.setOnClickListener {
+      coffeeRepo.increment()
+    }
   }
 }
+
