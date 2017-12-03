e4897454e575cbaaca129944bcbbe1ac62c70657
Victoria Gonda <v.m.gonda@gmail.com>
2017-12-03 15:20:41 -0500

    Update consumed when limit is changed

--- app/src/main/java/com/raywenderlich/android/coffeeoverflow/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/coffeeoverflow/MainActivity.kt
@@ -2,6 +2,8 @@ package com.raywenderlich.android.coffeeoverflow
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     showCount()
+
+    coffeeLimitValue.addTextChangedListener(object: TextWatcher {
+      override fun afterTextChanged(p0: Editable?) { }
+
+      override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+      override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = showCount()
+    })
 
     addCoffee.setOnClickListener {
       coffeeRepo.increment()
